521ce779f19ca6868097c0e89906789e56196fd6
EsraaSyam <e294014@gmail.com>
2024-05-04 16:06:01 +0300

    edit search route

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const {search_diseases, search_countries, search_vaccines , userSearch} = require('../Controllers/searchController');
 
 
-router.get('/search_diseases', search_diseases);
+router.post('/search_diseases', search_diseases);
 
-router.get('/search_countries', search_countries);
+router.post('/search_countries', search_countries);
 
-router.get('/search_vaccines', search_vaccines);
+router.post('/search_vaccines', search_vaccines);
 
-router.get('/userSearch', userSearch);
+router.post('/userSearch', userSearch);
 
 module.exports = router;
